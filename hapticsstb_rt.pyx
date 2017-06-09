a56d66981e0918b56aad9aa5e16ec670751eb461
zpatty <zpatt720@gmail.com>
2017-06-09 11:06:45 -0400

    Commented and organized

--- hapticsstb_rt.pyx
+++ hapticsstb_rt.pyx
@@ -12,7 +12,7 @@ PLOT_M40V = 2
 PLOT_ACC = 3
 PLOT_POS = 4
 
-# Calibrated linear transform for out Mini40, from channel voltages to forces
+# Calibrated linear transform for our Mini40, from channel voltages to forces
 # and torques
 
 M40_transform = np.array(  [[ 0.165175269, 	6.193716635,	-0.05972626,	0.020033203, 	-0.136667224, 	-6.42215241	],
@@ -22,17 +22,25 @@ M40_transform = np.array(  [[ 0.165175269, 	6.193716635,	-0.05972626,	0.02003320
 							[ -0.089833077,	-0.024635731,	0.165602185,	-0.009131771,	-0.080132747,	0.039589968	],
 							[ 0.001846317,	0.085776855,	0.005262967,	0.088317691, 	0.001450272,	0.087714269	]], dtype=np.float) 
 
-# Takes in a serial packet and 6-element bias vector, outputs a 6-element
-# vector with forces and torques
-# [Fx, Fy, Fz, Tx, Ty, Tz] = Serial2FT(packet, bias)
+"""
+Decodes serial packet and transforms information into the forces and torques read from the
+Mini40
 
-def serial_m40(str x, np.ndarray[np.float64_t, ndim = 1] bias):
-	volts = np.zeros((6), dtype = np.float64)
+@param pack raw data in unicode string form
+@param bias bias vector detected by the empty force plate
+@return 6-element vector with forces and torques [Fx, Fy, Fz, Tx, Ty, Tz]
+"""
+def serial_m40(str pack, np.ndarray[np.float64_t, ndim = 1] bias):
+	volts = np.zeros((6), dtype = np.float64) #intializing an array to all zeros for the data
 	cdef int i, j, y
 
 	for i in range(0,6):
 		j = i*2
-		y = (ord(x[j])<<8) + (ord(x[j+1]))
+		#Pack is 12 bit string and each set of two bytes is one data point
+		#Read in as two bytes, takes them in and concatenates them
+		y = (ord(pack[j])<<8) + (ord(pack[j+1]))
+
+		#If statement accounts for twos complement notation
 		if y > 2048:
 			volts[5-i] = <float>(y - 4096)*0.002
 		else:
@@ -41,10 +49,13 @@ def serial_m40(str x, np.ndarray[np.float64_t, ndim = 1] bias):
 	return np.dot(M40_transform, (volts - bias).T)
 
 
-# Takes serial packet, returns accelerometer voltages
-# [Acc1X, Acc1Y, Acc1Z, Acc2X, Acc2Y, Acc2Z, Acc3X, Acc3Y, Acc3Z] = Serial2Acc(packet)
+"""
+Decodes serial packet and transforms information into accelerometer voltages
 
-def serial_acc(str x):
+@param pack raw data in unicode string form
+@return 6-element vector with accelerometer voltages [Acc1X, Acc1Y, Acc1Z, Acc2X, Acc2Y, Acc2Z, Acc3X, Acc3Y, Acc3Z]
+"""
+def serial_acc(str pack):
 	gees = np.zeros((9), dtype = np.float64)
 	acc_order = [0,1,2,5,3,4,8,6,7] #Puts acc channels in x,y,z order
 
@@ -52,28 +63,39 @@ def serial_acc(str x):
 
 	for i in range(0,9):
 		j = (acc_order[i]+6)*2
-		y = (ord(x[j])<<8) + (ord(x[j+1]))
-		gees[i] = ((<float>y/1241)-1.65)*(15.0/3.3)
+		y = (ord(pack[j])<<8) + (ord(pack[j+1]))
+		gees[i] = ((<float>y/1241)-1.65)*(15.0/3.3) #Conversion factor
 
 	return gees
 
-# Takes serial packet and bias, calls other two functions and returns both
-# [Fx, Fy, Fz, Tx, Ty, Tz, Acc1X, Acc1Y, Acc1Z, Acc2X, Acc2Y, Acc2Z, Acc3X, Acc3Y, Acc3Z] = Serial2Data(packet, bias)
-def serial_data(str x, np.ndarray[np.float64_t, ndim = 1] bias):
-	FT = serial_m40(x, bias)
-	ACC = serial_acc(x)
+"""
+Convience function to perform both the m40 force readings and acceleromter voltage readings
+from a single packet of data
+
+@param pack raw data in unicode string form
+@param bias bias vector detected by the empty force plate
+@return 6-element vector with forces and torques [Fx, Fy, Fz, Tx, Ty, Tz]
+@return 6-element vector with accelerometer voltages [Acc1X, Acc1Y, Acc1Z, Acc2X, Acc2Y, Acc2Z, Acc3X, Acc3Y, Acc3Z]
+"""
+def serial_data(str pack, np.ndarray[np.float64_t, ndim = 1] bias):
+	FT = serial_m40(pack, bias)
+	ACC = serial_acc(pack)
 
 	return np.hstack((FT, ACC))
 
-# Takes serial packet, returns Mini40 Voltages
-# [V0, V1, V2, V3, V4, V5] = Serial2M40Volts(data)
+"""
+Decodes serial packet and transforms information into Mini40 voltages
 
-def serial_m40v(str x):
+@param pack raw data in unicode string form
+
+@return 6-element vector with voltages [V0, V1, V2, V3, V4, V5]
+"""
+def serial_m40v(str pack):
 	cdef int i, j, y
 	volts = np.zeros((6), dtype = np.float64)
 	for i in range(0,6):
 		j = i*2
-		y = (ord(x[j])<<8) + (ord(x[j+1]))
+		y = (ord(pack[j])<<8) + (ord(pack[j+1]))
 		if y > 2048: # This handles the twos complement negatives
 			volts[5-i] = <float>(y - 4096)*0.002
 		else:
